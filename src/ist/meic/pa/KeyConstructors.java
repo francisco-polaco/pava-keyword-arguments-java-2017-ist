80b3102d0630b351f8f6e60beb6715832e48710e
Francisco Santos <franciscopolaco@ist.utl.pt>
2017-03-26 16:44:30 +0100

    Some new tests, and a beautiful exception trace

--- src/ist/meic/pa/KeyConstructors.java
+++ src/ist/meic/pa/KeyConstructors.java
@@ -16,6 +16,19 @@ public class KeyConstructors {
         ClassPool classPool = ClassPool.getDefault();
         Loader classLoader = new Loader();
         classLoader.addTranslator(classPool, keyConstructorsTranslator);
-        classLoader.run(args[0], null);
+        // To show a trace as presented on the project description
+        try {
+            classLoader.run(args[0], null);
+        }catch (Throwable e){
+            RuntimeException ex = null;
+            while(e.getCause()!= null){
+                if(e.getCause() instanceof RuntimeException){
+                    ex = (RuntimeException) e.getCause();
+                }
+                e = e.getCause();
+            }
+            if(ex == null) throw e;
+            else throw ex;
+        }
     }
 }
---
+++ src/ist/meic/pa/test/TestFooBar.java
@@ -0,0 +1,8 @@
+package ist.meic.pa.test;
+
+public class TestFooBar {
+	public static void main(String[] args) {
+		System.err.println(new Foo());
+		System.err.println(new Bar());
+	}
+}
