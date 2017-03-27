0fa4ea31fcacbb1ee63e1d0c179efd0fa6483cc4
Francisco Santos <franciscopolaco@ist.utl.pt>
2017-03-27 19:14:19 +0100

    ai

--- Tests/lixo/TestLove.java
+++
@@ -1,9 +0,0 @@
-package ist.meic.pa.test.lixo;
-
-public class TestLove {
-	public static void main(String[] args) {
-		System.err.println(new Love());
-		System.err.println(new Love("heartBpm", 200));
-		System.err.println(new Love("personName", "DM")); // This should not be allowed
-	}
-}
